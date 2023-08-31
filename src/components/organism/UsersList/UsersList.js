efa4bef37a4e0a58f14516303150ce2453fca8dd
Robert Graupner <robert.graupnerr@gmail.com>
2023-08-31 20:55:17 +0200

    add delete user function

--- src/components/organism/UsersList/UsersList.js
+++ src/components/organism/UsersList/UsersList.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { users } from '../../../data/users';
 import shortid from 'shortid';
 import UsersListItem from '../../molecules/UsersListItem/UsersListItem';
 import styles from './UsersList.module.scss';
 
 const UsersList = () => {
+  const [user, setUser] = useState(users);
+  console.log(user);
+
+  const deleteUser = (id) => {
+    const filteredUsers = user.filter((user) => user.id !== id);
+    setUser(filteredUsers);
+  };
+
   return (
     <div className={styles.wrapper}>
       <ul className={styles.list}>
-        {users.map((user) => (
-          <UsersListItem user={user} key={shortid.generate()} />
+        {user.map((user) => (
+          <UsersListItem
+            deleteUser={deleteUser}
+            user={user}
+            key={shortid.generate()}
+          />
         ))}
       </ul>
     </div>
