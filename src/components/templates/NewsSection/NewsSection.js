b9683e8767a839d94ea65bc542233c1fdc464344
Robert Graupner <robert.graupnerr@gmail.com>
2023-10-07 20:44:24 +0200

    add generating article from array

--- src/components/templates/NewsSection/NewsSection.js
+++ src/components/templates/NewsSection/NewsSection.js
@@ -2,25 +2,71 @@ import React from 'react';
 import styles from './NewsSection.module.scss';
 import ViewWrapper from '../../molecules/ViewWrapper/ViewWrapper';
 import Button from '../../atoms/Button/Button';
+import shortid from 'shortid';
+
+const data = [
+  {
+    id: shortid.generate(),
+    title: 'New computers at school',
+    category: 'Tech news',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+  },
+  {
+    id: shortid.generate(),
+    title: 'New computers at school',
+    category: 'Tech news',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+  },
+  {
+    id: shortid.generate(),
+    title: 'New computers at school',
+    category: 'Tech news',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    image:
+      'https://images.unsplash.com/photo-1584713503693-bb386ec95cf2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80'
+  },
+  {
+    id: shortid.generate(),
+    title: 'New computers at school',
+    category: 'Tech news',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    image:
+      'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'
+  },
+  {
+    id: shortid.generate(),
+    title: 'New computers at school',
+    category: 'Tech news',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    image: null
+  },
+  {
+    id: shortid.generate(),
+    title: 'New computers at school',
+    category: 'Tech news',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    image:
+      'https://images.unsplash.com/photo-1613919517761-0d9e719d3244?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80'
+  }
+];
 
 const NewsSection = () => {
   return (
     <div className={styles.news}>
-      <h2 className={styles.sectionHeader}>News Section</h2>
-      <ViewWrapper className={styles.articleWrapper}>
-        <div className={styles.articleTitle}>
-          <h3>Lorem ipsum</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </div>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-          consequat ipsum in felis tristique elementum. Morbi vitae pellentesque
-          turpis. Donec bibendum nisi purus, ut sollicitudin tortor eleifend in.
-          Sed ullamcorper nisl et dictum faucibus. Curabitur magna justo,
-          blandit ut consequat vel, sodales et orci.
-        </p>
-        <Button>Read more</Button>
-      </ViewWrapper>
+      <h2 className={styles.sectionHeader}>University news section</h2>
+      {data.map((item) => (
+        <ViewWrapper className={styles.articleWrapper} key={item.id}>
+          <div className={styles.titleWrapper}>
+            <h3>{item.title}</h3>
+            <p>{item.category}</p>
+          </div>
+          <div className={styles.contentWrapper}>
+            <p>{item.content}</p>
+            {item.image ? <img src={item.image} alt='article' /> : null}
+          </div>
+          <Button>Read more</Button>
+        </ViewWrapper>
+      ))}
     </div>
   );
 };
